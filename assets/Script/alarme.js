7a9ad94dc05fb2837b9023ed41440f22b261a6a9
William Lago <williamlago2006@gmail.com>
2025-08-16 01:34:55 -0300

    Ajustando o Alarme e criando o Modo Escuro improvisado

--- assets/Script/alarme.js
+++ assets/Script/alarme.js
@@ -1,38 +1,54 @@
 const currenTime = document.querySelector('h2')
-const content = document.querySelectorAll('.content')
+const content = document.querySelector('.content')
 const selectMenu = document.querySelectorAll('select')
-const btnSetAlarm = document.querySelector('button')
+const btnSetAlarm = document.querySelector('#button-alarm')
 
 setInterval(()=>{
     let date = new Date()
-    let hora = FormataZero(date.getHours())
-    let minutos = FormataZero(date.getMinutes())
-    let segundos = FormataZero(date.getSeconds())
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    let ampm = "AM";
 
-    currenTime.innerHTML = `${hora}:${minutos}:${segundos}`
+    if( hours >= 12){
+        hours = hours -12
+        ampm = "PM"
+    }
 
-    if(alarmTime === `${hora}:${minutos}`){
+    hours = hours == 0 ? hours = 12 : hours
+
+    hours = hours < 10 ? "0" + hours : hours
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    seconds = seconds < 10 ? "0" + seconds : seconds
+
+    currenTime.innerHTML = `${hours}:${minutes}:${seconds} ${ampm}`
+
+    if(alarmTime === `${hours}:${minutes} ${ampm}`){
         ringTone.play()
         ringTone.loop = true
     }
 })
 
-let alarmTime, isAlarmSet, ringTone = new Audio("assets/audio/ringtone.mp3")
+let alarmTime, isAlarmSet, ringTone = new Audio("assets/audio/alarme clock_audio_ringtone.mp3")
 
-for(let i = 23; i>=0; i--){
-    i = i <10 ? `0${i}` : i
+for(let i=12; i>0; i--){
+    i = i < 10 ? "0" + i : i
     let option = ` <option value="${i}">${i}</option>`
-    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend",option)
+    selectMenu[0].firstElementChild.insertAdjacentHTML('afterend',option)
 }
 
-for(let i = 59; i>=0; i--){
-    i = i <10 ? `0${i}` : i
+for(let i=59; i>0; i--){
+    i = i < 10 ? "0" + i : i
     let option = ` <option value="${i}">${i}</option>`
-    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend",option)
+    selectMenu[1].firstElementChild.insertAdjacentHTML('afterend',option)
 }
 
-function FormataZero(num){
-    return num < 10 ? '0' + num : num
+
+for(let i=2; i>0; i--){
+    let ampm = i == 1 ? "AM" : "PM"
+    i = i < 10 ? "0" + i : i
+    let option = ` <option value="${ampm}">${ampm}</option>`
+    selectMenu[2].firstElementChild.insertAdjacentHTML('afterend',option)
 }
 
 function setAlarm(){
@@ -44,15 +60,15 @@ function setAlarm(){
         return isAlarmSet = false
     }
 
-
-    let time = `${selectMenu[0].value}:${selectMenu[1].value}`
-    if(time.includes("Hour") || time.includes("Minute")){
-        return alert("Insira horas e minutos válidos para ativar o alarme.")
+    let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`
+    if(time.includes("Hours") || time.includes("Minute") || time.includes("AM/PM")){
+        return alert("Insira horas e minutos válidos para ativar o alarme")
     }
+
     alarmTime = time
     isAlarmSet = true
     content.classList.add("disable")
     btnSetAlarm.innerHTML = "Desativar Alarme"
-}
 
-btnSetAlarm.addEventListener("click", setAlarm)
+}
+btnSetAlarm.addEventListener("click",setAlarm)
